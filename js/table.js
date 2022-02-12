1e347aedf110f7c24c5c02ad707ee70c921dfb3a
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-13 02:52:11 +0900

    220213_address

--- js/table.js
+++ js/table.js
@@ -1,3 +1,9 @@
 const tableData = document.getElementsByTagName("td");
-
-console.dir(tableData);
+const dayOff = "";
+const dayOffColor = "#f39800";
+for (let i = 0; i < tableData.length; i++) {
+  if (tableData[i].innerText === dayOff) {
+    tableData[i].innerText = "●";
+    tableData[i].style.color = dayOffColor;
+  }
+}
