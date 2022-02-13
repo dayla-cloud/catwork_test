0307fb3d81f94a0a58b0b3bec2a04bec9d1d5f9f
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-13 20:51:55 +0900

    220213_final

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+gulp.task("default", function () {
+  return gulp
+    .src("css/styles.css")
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(gulp.dest("finalStylesCss"));
+});
