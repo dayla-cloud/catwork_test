c708c1d6a9516dc55754a5aaeb4289be0a6de402
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-13 23:42:53 +0900

    220213_fin_real

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,19 @@ gulp.task("default", function () {
     .pipe(
       postcss([
         autoprefixer({
-          browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+          browsers: [
+            "> 1%",
+            "ie >= 8",
+            "edge >= 15",
+            "ie_mob >= 10",
+            "ff >= 45",
+            "chrome >= 45",
+            "safari >= 7",
+            "opera >= 23",
+            "ios >= 7",
+            "android >= 4",
+            "bb >= 10",
+          ],
           cascade: false,
         }),
       ])
