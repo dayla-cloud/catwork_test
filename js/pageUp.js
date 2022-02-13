189d754fda7f42dc2118bad38915691d8a4d2b9c
dayla Kim <ovoa.ovoz@gmail.com>
2022-02-13 22:57:22 +0900

    220213_fin_add_animation

--- js/pageUp.js
+++ js/pageUp.js
@@ -1,7 +1,21 @@
 const btnPageUp = document.getElementById("btn-page-top");
-
+const landingPage = document.getElementById("landing-page");
+const landingPageHalfHeight = landingPage.offsetHeight / 2;
 function scrollUp() {
   window.scroll(0, 0);
 }
 
+function showBtnPageUp() {
+  const currentScrollLocation = window.scrollY;
+  console.log(currentScrollLocation);
+  if (currentScrollLocation >= landingPageHalfHeight) {
+    btnPageUp.style.bottom = "194px";
+    btnPageUp.style.opacity = "1";
+  } else {
+    btnPageUp.style.bottom = "-71px";
+    btnPageUp.style.opacity = "0";
+  }
+}
+
+window.addEventListener("scroll", showBtnPageUp);
 btnPageUp.addEventListener("click", scrollUp);
